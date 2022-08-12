7e8d8a6a0cd32db79ef26eeab980fedfaa955cbc
Lucas <77863766+i-am-lucas-almeida@users.noreply.github.com>
2022-08-12 15:30:54 -0300

    Update usePagination.js

--- src/hook/usePagination.js
+++ src/hook/usePagination.js
@@ -32,7 +32,7 @@ export default function usePagination() {
                 ...queryParams,
                 page: actualPage
             })
-            
+
         });
 
     }, [actualPage, navigate, location.search]);
